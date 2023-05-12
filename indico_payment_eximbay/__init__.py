396773e983f196104dae1a63019f2868b65a0a87
Gyujin Kim <ilyoukim@postech.ac.kr>
2023-05-12 11:11:58 +0900

    update gettext

--- indico_payment_eximbay/__init__.py
+++ indico_payment_eximbay/__init__.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 ##
 ## This file is part of the Eximbay Indico EPayment Plugin.
-## Copyright (C) 2019 - 2020 Gyujin Kim
+## Copyright (C) 2019 - 2023 Gyujin Kim
 ##
 ## This is free software; you can redistribute it and/or
 ## modify it under the terms of the GNU General Public License as
@@ -15,3 +15,8 @@
 ##
 ## You should have received a copy of the GNU General Public License
 ## along with Eximbay Indico EPayment Plugin;if not, see <http://www.gnu.org/licenses/>.
+
+from indico.util.i18n import make_bound_gettext
+
+
+_ = make_bound_gettext('payment_eximbay')
