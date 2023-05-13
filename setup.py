65e2bfeb78448b5e707cf75f3ca97d5140e1058d
Gyujin Kim <ilyoukim@postech.ac.kr>
2023-05-13 17:38:51 +0900

    fix setup

--- setup.py
+++ setup.py
@@ -4,8 +4,45 @@
 # The Indico plugins are free software; you can redistribute
 # them and/or modify them under the terms of the MIT License;
 # see the LICENSE file for more details.
+import os
+from setuptools import setup, find_packages
 
-from setuptools import setup
+repo_base_dir = os.path.abspath(os.path.dirname(__file__))
+# pull in the packages metadata
+package_about = {}
+with open(os.path.join(repo_base_dir, "indico_payment_eximbay", "__about__.py")) as about_file:
+    exec(about_file.read(), package_about)
 
-
-setup()
+setup(
+    name=package_about['__title__'],
+    version=package_about['__version__'],
+    description=package_about['__summary__'],
+    long_description=package_about['__doc__'].strip(),
+    author=package_about['__author__'],
+    author_email=package_about['__email__'],
+    url=package_about['__url__'],
+    entry_points={
+        'indico.plugins': {
+            'payment_eximbay = indico_payment_eximbay.plugin:EximbayPaymentPlugin'
+        }
+    },
+    packages=find_packages(),
+    package_data={'indico_payment_eximbay': ['templates/*.html','static/images/*.png']},
+    install_requires=['requests', 'indico>=3.0', 'iso4217'],
+    license='GPLv3+',
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'Environment :: Plugins',
+        'Environment :: Web Environment',
+        'Intended Audience :: Science/Research',
+        'Intended Audience :: System Administrators',
+        'Intended Audience :: Other Audience',
+        'License :: OSI Approved :: GNU General Public License v3 or later (GPLv3+)',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python :: 3.9',
+        'Topic :: Communications :: Conferencing',
+        'Topic :: Internet :: WWW/HTTP :: WSGI :: Application',
+    ],
+    zip_safe=False,
+    keywords='indico epayment exim eximbay plugin',
+)
