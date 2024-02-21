7b15ccf3eeef78c80c0e1b37fb74ad9c957fa6dd
Gyujin Kim <ilyoukim@postech.ac.kr>
2024-02-21 16:22:51 +0900

    add card name

--- indico_payment_eximbay/util.py
+++ indico_payment_eximbay/util.py
@@ -37,6 +37,77 @@ EXIMBAY_CURRENCY = {'KRW','USD','EUR','GBP','JPY','THB','SGD','RUB','HKD','CAD',
 # Support Language : Eximbay manual - Appendix B
 EXIMBAY_LANGUAGE = {'KR','EN','CN','JP','RU','TH','TW','VN'}
 
+EXIMBAY_PAYMETHOD = {
+    "P000": "Credit Card",
+    "P101": "VISA",
+    "P102": "MasterCard",
+    "P103": "AMEX",
+    "P104": "JCB",
+    "P105": "CUP(UnionPay 2D)",
+    "P106": "Diners",
+    "P107": "Discover",
+    "P108": "Mir",
+    "P001": "PayPal",
+    "P002": "CUP(UnionPay)",
+    "P003": "Alipay or Alipay Plus",
+    "P174": "Alipay Plus(Alipay_CN)",
+    "P175": "Alipay Plus(TRUEMONEY)",
+    "P176": "Alipay Plus(DANA)",
+    "P177": "Alipay Plus(Alipay_HK)",
+    "P178": "Alipay Plus(TNG)",
+    "P179": "Alipay Plus(GCASH)",
+    "P141": "WeChat(PC)",
+    "P142": "WeChat(Mobile)",
+    "P143": "WeChat(POP)",
+    "P144": "WeChat(MINI)",
+    "P006": "Japanese Convenience Store, Internet Banking Payment",
+    "P171": "Razer Merchant Services(Malaysia)",
+    "P172": "Razer Merchant Services(Vietnam)",
+    "P173": "Razer Merchant Services(Thailand)",
+    "P011": "YooMoney",
+    "PG01": "2C2P",
+    "P185": "GrabPay(SGD)",
+    "P189": "GrabPay(MYR)",
+    "P190": "GrabPay(PHP)",
+    "P186": "LinePay(2C2P)",
+    "P194": "LinePay(eContext)",
+    "P110": "BC Card",
+    "P111": "KB Card",
+    "P112": "HANA Card",
+    "P113": "SAMSUNG Card",
+    "P114": "SHINHAN Card",
+    "P115": "HYUNDAI Card",
+    "P116": "LOTTE Card",
+    "P117": "NONGHYEOP Card",
+    "P119": "CITI Card",
+    "P120": "WOORI Card",
+    "P121": "SUHYEOP Card",
+    "P122": "JEJU Card",
+    "P123": "JEONBUK Card",
+    "P124": "GWANGJU Card",
+    "P125": "KAKAOBANK",
+    "P126": "KBANK",
+    "P127": "MIRAEASSET",
+    "P128": "KONA Card",
+    "P129": "TOSS Card",
+    "P130": "CHAI Card",
+    "P301": "BANK PAY",
+    "P302": "KAKAO PAY",
+    "P303": "TOSS",
+    "P304": "PAYCO",
+    "P305": "Virtual Account",
+    "P306": "SMILE PAY",
+    "P015": "NAVER PAY(CARD & POINT)",
+    "P307": "NAVER PAY(CARD)",
+    "P308": "NAVER PAY(POINT)",
+}
+
+
+def get_paymethod(code):
+    if code in EXIMBAY_PAYMETHOD:
+        return EXIMBAY_PAYMETHOD.get(code)
+    else:
+        return code
 
 def get_fgkey(exb_secret, data):
     """Specific function for Eximbay
