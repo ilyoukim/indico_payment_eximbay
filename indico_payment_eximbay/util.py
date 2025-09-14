9f52cb9999bfbad8f4c236657f3124fc09b5ea54
Gyujin Kim <ilyoukim@postech.ac.kr>
2025-09-14 16:23:42 +0900

    update api code list

--- indico_payment_eximbay/util.py
+++ indico_payment_eximbay/util.py
@@ -24,7 +24,8 @@ from indico_payment_eximbay import _
 # payment provider identifier
 PROVIDER_EXIMBAY = 'eximbay'
 
-# Eximbay API details
+
+# Eximbay API URLs
 EXIMBAY_SERVICE_DOMAIN = "https://api.eximbay.com"
 
 EXIMBAY_SDK_URL = "v2/javascriptSDK.js"
@@ -34,27 +35,30 @@ EXIMBAY_READY_URL = "v1/payments/ready"
 EXIMBAY_CONFIRM_URL = "v1/payments/confirm"
 EXIMBAY_VERIFY_URL= "v1/payments/verify"
 EXIMBAY_RETRIEVE_URL = "v1/payments/retrieve"
-EXIMBAY_CANCEL_URL = "v1/payments/{transaction_id}/cancel"
+EXIMBAY_CANCEL_URL = "v1/payments/<transaction_id>/cancel"
 
 
+# https://developer.eximbay.com/eximbay/api_sdk/code-etc.html
 # Support Currency : Eximbay manual - Appendix A
 EXIMBAY_CURRENCY = {'KRW','USD','EUR','GBP','JPY','THB','SGD','RUB','HKD','CAD','AUD'}
 
 # Support Language : Eximbay manual - Appendix B
 EXIMBAY_LANGUAGE = {'KR','EN','CN','JP','RU','TH','TW','VN'}
 
+
+# https://developer.eximbay.com/eximbay/api_sdk/code-organization.html
 EXIMBAY_PAYMETHOD = {
     "P000": "Credit Card",
     "P101": "VISA",
     "P102": "MasterCard",
     "P103": "AMEX",
     "P104": "JCB",
-    "P105": "CUP(UnionPay 2D)",
     "P106": "Diners",
     "P107": "Discover",
     "P108": "Mir",
+    "P109": "UnionPay",
     "P001": "PayPal",
-    "P002": "CUP(UnionPay)",
+    "P002": "CUP(UPOP)",
     "P003": "Alipay or Alipay Plus",
     "P174": "Alipay Plus(Alipay_CN)",
     "P175": "Alipay Plus(TRUEMONEY)",
@@ -66,17 +70,13 @@ EXIMBAY_PAYMETHOD = {
     "P142": "WeChat(Mobile)",
     "P143": "WeChat(POP)",
     "P144": "WeChat(MINI)",
-    "P006": "Japanese Convenience Store, Internet Banking Payment",
-    "P171": "Razer Merchant Services(Malaysia)",
-    "P172": "Razer Merchant Services(Vietnam)",
-    "P173": "Razer Merchant Services(Thailand)",
-    "P011": "YooMoney",
-    "PG01": "2C2P",
-    "P185": "GrabPay(SGD)",
-    "P189": "GrabPay(MYR)",
-    "P190": "GrabPay(PHP)",
-    "P186": "LinePay(2C2P)",
-    "P194": "LinePay(eContext)",
+    "P006": "ECONTEXT",
+    "P197": "Klarna",
+    "P350": "GrabPay(MYR)",
+    "P351": "GrabPay(SGD)",
+    "P352": "ShopeePay(THB)",
+    "P353": "JKOPAY(TWD)",
+    "P354": "PayPay",
     "P110": "BC Card",
     "P111": "KB Card",
     "P112": "HANA Card",
@@ -93,7 +93,7 @@ EXIMBAY_PAYMETHOD = {
     "P124": "GWANGJU Card",
     "P125": "KAKAOBANK",
     "P126": "KBANK",
-    "P127": "MIRAEASSET",
+    "P127": "MIRAE ASSET",
     "P128": "KONA Card",
     "P129": "TOSS Card",
     "P130": "CHAI Card",
@@ -102,7 +102,6 @@ EXIMBAY_PAYMETHOD = {
     "P303": "TOSS",
     "P304": "PAYCO",
     "P305": "Virtual Account",
-    "P306": "SMILE PAY",
     "P015": "NAVER PAY(CARD & POINT)",
     "P307": "NAVER PAY(CARD)",
     "P308": "NAVER PAY(POINT)",
@@ -111,6 +110,7 @@ EXIMBAY_PAYMETHOD = {
 
 def get_paymethod(code):
     EXIMBAY_PAYMETHOD.get(code, code)
+
 
 def get_request_header(api_key):
     """Base64 encoding
