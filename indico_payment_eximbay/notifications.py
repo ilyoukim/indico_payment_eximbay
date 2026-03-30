40f7347bb3d13dae130fe7749183d5e0358208f7
Gyujin Kim <ilyoukim@postech.ac.kr>
2026-03-30 11:22:10 +0900

    refactor: update notify_payment_error function to improve address handling and template selection

--- indico_payment_eximbay/notifications.py
+++ indico_payment_eximbay/notifications.py
@@ -10,6 +10,7 @@ from indico.web.flask.templating import get_template_module
 
 from indico_payment_eximbay.util import get_paymethod
 
+
 @email_sender
 def notify_account_error(registration, data, to_address):
     event = registration.registration_form.event
@@ -18,10 +19,12 @@ def notify_account_error(registration, data, to_address):
     with event.creator.force_user_locale():
         tpl = get_template_module('payment_eximbay:emails/account_error_notify.html',
                                   event=event, registration=registration, data=data)
+        
         return make_email(to_address, template=tpl, html=True)
 
+
 @email_sender
-def notify_payment_error(registration, data, to_address=None):
+def notify_payment_error(registration, data, manager_address=None):
     # Define the allowed keys for the notification data
     allowed_keys = {
         'order_id', 'amount', 'currency', 'access_country', 'email',
@@ -37,16 +40,17 @@ def notify_payment_error(registration, data, to_address=None):
     
     paymethod = get_paymethod(data.get('payment_method',''))
 
-    if to_address:
+    if manager_address:
         template_file = 'payment_error_notify_manager.html'
+        to_address = manager_address
     else:
         template_file = 'payment_error_notify_register.html'
-        to_address = (registration.email, data.get('email'))
-        to_address = list(set(to_address))
+        to_address = list({addr for addr in (registration.email, data.get('email', '')) if addr})
     
     with event.creator.force_user_locale():
         tpl = get_template_module('payment_eximbay:emails/' + template_file,
                                     event=event, registration=registration, data=newdata,
                                     paymethod=paymethod, orderID=data.get('order_id',''),
                                     errCode=data.get('rescode',''), errMsg=data.get('resmsg',''))
+        
         return make_email(to_address, template=tpl, html=True)
